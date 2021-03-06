73ae13eb253b266a104d557b00ecf78257e092c8
yichigo <zycliamg@gmail.com>
2021-03-05 21:57:59 -0600

    traffic light

--- Concurrency.py
+++ Concurrency.py
@@ -27,3 +27,32 @@ class Foo:
             # printThird() outputs "third". Do not change or remove this line.
             printThird()
 
+
+class TrafficLight:
+    def __init__(self):
+        self.openRoad = 1
+        self.lock = Lock() # 1 for A, 2 for B
+        self.direction2road = {1:1,2:1,3:2,4:2}
+
+    def carArrived(
+        self,
+        carId: int,                      # ID of the car
+        roadId: int,                     # ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+        direction: int,                  # Direction of the car
+        turnGreen: 'Callable[[], None]', # Use turnGreen() to turn light to green on current road
+        crossCar: 'Callable[[], None]'   # Use crossCar() to make car cross the intersection
+    ) -> None:
+        
+        if direction:
+            roadId = self.direction2road[direction]
+        
+        if roadId == self.openRoad: # cross
+            with self.lock:
+                crossCar()
+        else: # wait
+            with self.lock:
+                self.openRoad = roadId
+                turnGreen()
+                crossCar()
+
+                
